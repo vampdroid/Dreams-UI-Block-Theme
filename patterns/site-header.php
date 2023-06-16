3bfd41bffff4a1205af93d17ba1e3c655821d7f6
Yash Kukreja <yashskukreja@gmail.com>
2023-06-16 13:44:49 +0530

    Add site-header block pattern

---
+++ patterns/site-header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Site Header
+ * slug: dui/site-header
+ * categories: dreams-ui
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"2.5rem","bottom":"2.5rem"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained","contentSize":"1000px"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:2.5rem;padding-bottom:2.5rem">
+    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"},"fontSize":"small"} -->
+    <div class="wp-block-group has-small-font-size">
+    
+    <!-- wp:site-logo /-->
+
+    <!-- wp:navigation /-->
+
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
